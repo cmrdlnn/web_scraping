ad817aed982146cd356ed4f86df87f783f9d8f73
Nikolay Ulyanov <cmrdlnn@gmail.com>
2018-06-02 16:54:42 +0300

    Fix of Calender component

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 const devServerOptions = process.argv.find(arg => arg.includes('webpack-dev-server'))
   ? {
     devServer: { headers: { 'Access-Control-Allow-Origin': '*' } },
-    output: { publicPath: '/' },
+    output: { publicPath: 'http://0.0.0.0:8080/' },
   }
   : {};
 
@@ -31,7 +31,10 @@ module.exports = merge({
       },
     ],
   },
-  output: { path: path.join(__dirname, 'public') },
+  output: {
+    path: path.join(__dirname, 'public'),
+    publicPath: '/',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       alwaysWriteToDisk: true,
